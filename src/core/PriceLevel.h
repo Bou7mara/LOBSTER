a3b7f04944876a083194201b8cae027739a53891
Bou7mara <3a.hmz.992@gmail.com>
2026-08-18 14:46:24 +0100

    feat(core): implement PriceLevel intrusive linked list

---
+++ src/core/PriceLevel.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Order.h"
+#include "Types.h"
+
+struct PriceLevel {
+    Price price = 0;
+    Order* head = nullptr;
+    Order* tail = nullptr;
+
+    void pushBack(Order* order) {
+        order->prev = tail;
+        order->next = nullptr;
+
+        if (tail != nullptr) {
+            tail->next = order;
+        }
+        else {
+            head = order;
+        }
+
+        tail = order;
+    }
+
+    void unlink(Order* order) {
+        if (order->prev != nullptr) {
+            order->prev->next = order->next;
+        }
+        else {
+            head = order->next;
+        }
+
+        if (order->next != nullptr) {
+            order->next->prev = order->prev;
+        }
+        else {
+            tail = order->prev;
+        }
+
+        order->prev = nullptr;
+        order->next = nullptr;
+    }
+};
